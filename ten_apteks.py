68aafc5f5e4db2e0d48ef18a2291c4e84721cbef
schigolefff <74930889+schigolefff@users.noreply.github.com>
2021-02-06 14:54:33 +0300

    Add files via upload

--- ten_apteks.py
+++ ten_apteks.py
@@ -1,22 +1,28 @@
+import argparse
 import requests
 
 from show_map import show_map
+from get_ll_spn import get_ll_spn
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("address", type=str, nargs="*")
+    args = parser.parse_args()
+    address = " ".join(args.address)
+
     search_api_server = "https://search-maps.yandex.ru/v1/"
     api_key = "2398bcb3-ab43-4f54-85b6-f217451248a5"
+    ll = get_ll_spn(address)
     search_params = {
         "apikey": api_key,
         "text": "аптека",
         "lang": "ru_RU",
-        "ll": "37.588392,55.734036",
+        "ll": ll[0],
         "type": "biz"
     }
     response = requests.get(search_api_server, params=search_params)
     json_response = response.json()
-    organizations = json_response["features"]
-    if len(organizations) > 10:
-        organizations = organizations[:10]
+    organizations = json_response["features"][:10]
 
     orgs = {}
 
